2f2f4b23196d74827aad5f6954135ca9f71538aa
Galib6 <asadullahalgalib81@gmail.com>
2022-10-27 23:30:02 +0600

    last

--- src/Components/GetPermiumAccess/GetPremium.js
+++ src/Components/GetPermiumAccess/GetPremium.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Courses from '../Courses/Courses';
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 const GetPremium = () => {
 
     const details = useLoaderData()
     console.log(details)
     return (
-        <div>
+        <div ref={ref} style={{ width: 900, height: 500 }}>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase sm:text-center">
                     Tech Bee
@@ -25,10 +26,15 @@ const GetPremium = () => {
                             {details?.title}
                         </a>
                     </div>
+                    <p>Your name:</p>
+                    <input type="text" placeholder="Your Name" className="input input-bordered input-secondary w-full max-w-xs my-10" />
                     <p className="text-base text-gray-700 md:text-lg">
                         Thank you! Please start leaning form YOU COURSES section
                     </p>
                 </div>
+                <Pdf targetRef={ref} filename="code-example.pdf" className="flex">
+                    {({ toPdf }) => <button className="btn btn-sm" onClick={toPdf}>Generate Pdf</button>}
+                </Pdf>
             </div>
         </div>
     );
